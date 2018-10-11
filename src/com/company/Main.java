c10dc8a71c1ae60fbc106ee70bec7b97ebc1e85e
GBTC406005UR <GBTC406005UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-11 16:30:42 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+//Get the average of 3 numbers.
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+        System.out.println(getAverage());
+    }
+
+
+    public static int getAverage() {
+        int num1 = 3;
+        int num2 = 3;
+        int num3 = 6;
+
+            int sum = (num1 + num2 + num3);
+            int average = (sum / 3);
+
+            //System.out.println(average);
+            return average;
+
+        }
+
+
+
+}
+
+
+
+
+
